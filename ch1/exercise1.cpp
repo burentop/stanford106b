d7de1cb45e039fb06acb46f0bb16504f3d958bbc
Brent Perry <burentoperii@gmail.com>
2017-02-03 17:52:53 -0600

    initial commit.

---
+++ ch1/exercise1.cpp
@@ -0,0 +1,12 @@
+// Exercise 1 - Prompt the user for a Celsius temp, and output the Fahrenheit conversion
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Please enter a temperature in Celsius: ";
+    double celsius;
+    cin >> celsius;
+    double fahrenheit = 
+}
